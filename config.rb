3d1467c88a872c348d3590d55ff8c47b343bfb77
Jon Phillips <jonathan.phillips@livingsocial.com>
2013-01-23 10:11:50 -0500

    first deploy

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/elguapo1611/"
+http_images_path = "/elguapo1611/images"
+http_fonts_path = "/elguapo1611/fonts"
+css_dir = "public/elguapo1611/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
